50816bcdbc85779e17c8c614c7917aa29ca536bf
Micah-Wong1 <Wongm@educ.college-montreal.qc.ca>
2025-10-17 12:04:35 -0400

    ARGGGG its just a wannabe street trash

--- TP3.py
+++ TP3.py
@@ -3,11 +3,13 @@ Nom = Micah
 Gr = 406
 Ce code cree une scenario ou le jouer doit combattre des monstres une apres l'autre,
 et  chaque 3 victoires, un boss de haut difficulte apparaitre,
-le but du jeu est dele de survie le plus longtemps possible
+le but du jeu est dele de survie le plus longtemps possible avec 30 HP au depart
 """
 import random
 jeu = True
 
+dice_throw = 0
+second_throw = 0
 force_ennemi = 0
 boss_appear = 0
 numero_combat = 0
@@ -15,7 +17,7 @@ nombre_victoires = 0
 nombre_defaites = 0
 nbr_victoire_consecutive = 0
 combat_statut = "-"
-nbr_vie = 20
+nbr_vie = 30
 showed_rules = False
 
 
@@ -27,10 +29,10 @@ def force_pers(low, high):
 while jeu:
     if not showed_rules:
         if boss_appear % 3 == 0 and boss_appear != 0:
-            force_ennemi = force_pers(7, 12)
+            force_ennemi = force_pers(9, 24)
             print(f"Vous tombez face à face avec un boss de haut difficulté : {force_ennemi}")
         else:
-            force_ennemi = force_pers(1, 5)
+            force_ennemi = force_pers(2, 11)
             print(f"Vous tombez face à face avec un adversaire de difficulté : {force_ennemi}")
     else:
         if boss_appear % 3 == 0 and boss_appear != 0:
@@ -48,14 +50,18 @@ while jeu:
     if rep == 1:
         if boss_appear % 3 == 0 and boss_appear != 0:
             dice_throw = force_pers(1, 12)
+            second_throw = force_pers(1, 12)
         else:
             dice_throw = force_pers(1, 6)
+            second_throw = force_pers(1, 6)
         numero_combat += 1
         print(f"""Combat {numero_combat}: nombre de victores:{nombre_victoires} vs nombres de defaites:{nombre_defaites}
 Niveau de vie de l’usager: {nbr_vie}
 Force de l’adversaire: {force_ennemi}""")
-        print(f"Lancer du dé : {dice_throw} ")
-        if dice_throw <= force_ennemi:
+        print(f"""Lancer du dé : {dice_throw} 
+Lancer du deuxieme dé : {second_throw}
+Force total du jouer : {dice_throw + second_throw}""")
+        if dice_throw + second_throw <= force_ennemi:
             if boss_appear % 3 == 0 and boss_appear != 0:
                 boss_appear = 0
             nombre_defaites += 1
@@ -65,7 +71,7 @@ Force de l’adversaire: {force_ennemi}""")
             print(f"Derniere combat = {combat_statut}")
             print(f"Niveau de vie = {nbr_vie}")
 
-        elif dice_throw > force_ennemi:
+        elif dice_throw + second_throw > force_ennemi:
             boss_appear += 1
             nombre_victoires += 1
             nbr_vie += force_ennemi
@@ -82,8 +88,8 @@ Force de l’adversaire: {force_ennemi}""")
 Niveau de vie = {nbr_vie}""")
             boss_appear = 0
         else:
-            nbr_vie -= 1
-            print(f"""Il y a une pénalité de 1 point de vie. 
+            nbr_vie -= 2
+            print(f"""Il y a une pénalité de 2 point de vie. 
 Niveau de vie = {nbr_vie}""")
 
     elif rep == 3:
@@ -98,7 +104,7 @@ Niveau de vie = {nbr_vie}""")
         La partie se termine lorsque les points de vie de l’usager tombent sous 0.
 
         L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, 
-        il y a une pénalité de 1 point de vie. Si l'usager evite un boss, il y a une penalite de 7 point de vie
+        il y a une pénalité de 2 point de vie. Si l'usager evite un boss, il y a une penalite de 7 point de vie
         
         """)
 
@@ -108,15 +114,15 @@ Niveau de vie = {nbr_vie}""")
 
     if nbr_vie <= 0:
         combat_statut = "-"
-        print(f"Vous avez {nbr_vie} de vie")
+        print(f"Vous n'avez plus de vie")
         print(f"La partie est terminée, vous avez vaincu {nombre_victoires} monstre(s).")
         reply = str(input("voulez-vous redémarrer? (y/n)"))
         if reply == "y":
-            nbr_vie = 20
+            nbr_vie = 30
             nombre_defaites = 0
             nombre_victoires = 0
             numero_combat = 0
-            print("vous avez redémarré avec 20 points de vie")
+            print("vous avez redémarré avec 30 points de vie")
             print(f"Derniere combat = {combat_statut}")
             print(f"Niveau de vie = {nbr_vie}")
         elif reply == "n":
